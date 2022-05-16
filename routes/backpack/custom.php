c4020c1b108234610415c6eee639f1a6f7d36ba4
Hidde -_- <hiddealberts@gmail.com>
2022-05-16 10:32:27 +0200

    done customer crud and added some more crud pages

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -17,4 +17,7 @@ Route::group([
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
     Route::crud('order', 'OrderCrudController');
+    Route::crud('customer', 'CustomerCrudController');
+    Route::crud('food-category', 'FoodCategoryCrudController');
+    Route::crud('reservation', 'ReservationCrudController');
 }); // this should be the absolute last line of this file
