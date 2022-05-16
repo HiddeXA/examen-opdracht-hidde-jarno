3fb5f95c384f0bbf3c2d600912ad1dab3fa28909
Jarno <jj.waijerink@student.alfa-college.nl>
2022-05-16 10:34:17 +0200

    language change 2

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'App\Http\Controllers'], function () {
 
     Route::get('/', 'FrontEndController@viewHome');
-    Route::get('/drank', 'FrontEndController@drink');
-    Route::get('/gerechten', 'FrontEndController@dish');
+    Route::get('/drinks', 'FrontEndController@drink');
+    Route::get('/dishes', 'FrontEndController@dish');
 });
